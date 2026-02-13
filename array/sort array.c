091cc7026fc47381e744cc9b3dccc9a66b3ec69e
Bharath <chandranbharath02@gmail.com>
2026-02-13 13:23:16 +0530

    Add files via upload

---
+++ array/sort array.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+int a[5]={2,3,5,4,1};
+
+for(int i=0;i<5;i++)
+{
+for(int j=i+1;j<5;j++)
+{
+if(a[i]>a[j])
+{
+  int temp=a[i];
+      a[i]=a[j];
+      a[j]=temp;
+}
+}
+}
+for(int i=0; i<5; i++){
+printf("%d",a[i]);
+}
+return 0;
+}
