8b693be169761389d6a65ab7c1b36e4972440c71
Bharath <chandranbharath02@gmail.com>
2026-02-16 08:52:21 +0530

    Create pointer.typedef.c

---
+++ Structure&unions&enum/pointer.typedef.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+typedef int*intptr;
+
+void swap(intptr a,intptr b)
+{
+int  temp=*a;
+     *a=*b;
+     *b=temp;
+}
+int main(){
+int x=10;
+int y=20;
+
+printf("before swap: x=%d y=%d\n",x,y);
+
+swap(&x,&y);
+
+printf("after swap: x=%d y=%d\n", x,y);
+
+return 0;
+}
