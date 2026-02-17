e3f1ac204dcb7a14a309f43edbff2137295231cf
Bharath <chandranbharath02@gmail.com>
2026-02-17 15:49:41 +0530

    Add files via upload

---
+++ Structure&unions&enum/enum.custom.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+enum custom{
+star=10,
+stop=20,
+puase=30,
+};
+
+int main(){
+int a;
+enum custom choise;
+printf("enter a number");
+scanf("%d",&a);
+choise=(enum custom)a;
+if(choise==star)
+{
+printf("%d",star);
+}
+return 0;
+}
