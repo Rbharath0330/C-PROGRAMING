0cb07f0b4e3a8b3393e48141552f7391936be100
Bharath <chandranbharath02@gmail.com>
2026-03-19 15:02:57 +0530

    Add files via upload

---
+++ makefile/read_write/file_ops.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "file_ops.h"
+
+void read_file(){
+
+printf("content of file:\n");
+printf("sucess starts with every challenge \n not from the \ncomfort zone.");
+}
+
+void write_file(char fw[]){
+getchar();
+printf("enter bio-data\n");
+fgets(fw, 100, stdin);
+}
