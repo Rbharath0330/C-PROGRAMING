7bcddeccc208ef78be7840073a5d46113c71d1d2
Bharath <chandranbharath02@gmail.com>
2026-03-21 22:58:37 +0530

    Add files via upload

---
+++ practic/ed.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main(){
+
+int a[10]={1,2,3,4,5,6,7,8,9,10};
+
+int i,odd=0,even=0;
+for(i=0;i<10;i++){
+if(a[i]%2==0){
+even++;
+}
+else{
+odd++;
+}
+}
+printf("even %d\n",even);
+printf("odd %d\n",odd);
+
+return 0;
+}
+
