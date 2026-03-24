93c37007f392716f8538a407a14ecc63603a787c
Bharath <chandranbharath02@gmail.com>
2026-03-24 18:38:07 +0530

    Add files via upload

---
+++ c basic/bharath.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main(){
+
+int n,i;
+int sum=0;
+printf("Enter a integer");
+scanf("%d",&n);
+for(i=1;i<=n;++i)
+{
+  sum+=1;
+}
+   printf("sum=%d ",sum);
+
+return 0;
+}
+
