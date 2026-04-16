faa0699e5753132384e9a9470285d4533b7228c5
Bharath <chandranbharath02@gmail.com>
2026-04-16 23:06:15 +0530

    Add files via upload

---
+++ practic/swap_pointer.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x = 5, y = 10;
+
+    printf("%d %d\n", x, y);
+
+    swap(&x, &y);
+
+    printf("%d %d\n", x, y);
+
+    return 0;
+}
